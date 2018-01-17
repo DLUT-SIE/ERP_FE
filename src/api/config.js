d4eb3dc9f3ef49b1c48b1249e403599605e59b72
wanglei.77 <wanglei.77@bytedance.com>
2018-01-17 19:33:09 +0800

    fix: 修改接口名
    Change-Id: I1be0e598cf6aa62a6ac311a6495f5ff8630f46e2

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-import Distribution from './distribution.api'
+import DistributionAPI from './distribution.api'
 import ProcessAPI from './process.api'
 import PurchaseAPI from './purchase.api'
 import InventoryAPI from './inventory.api'
@@ -9,7 +9,7 @@ const baseAPIs = {}
 // TODO:后续随 API 增加，建议根据功能拆分成单独文件维护
 // 拆分 API 在此处 assign 进来，如 Object.assign({}, baseAPIs, { moreAPIs })
 export const apis = Object.assign({}, baseAPIs, {
-  Distribution,
+  DistributionAPI,
   ProcessAPI,
   PurchaseAPI,
   InventoryAPI,
