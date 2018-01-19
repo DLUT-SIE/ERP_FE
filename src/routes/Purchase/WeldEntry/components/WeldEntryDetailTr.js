f78dae42dd09e58660e712a2ca913b2f67e9fc74
wanglei.77 <wanglei.77@bytedance.com>
2018-01-19 16:51:24 +0800

    fix: 将ItemOperationArea修改为公共组件
    Change-Id: I02b22e0252f5305ac0c3cfe899160e933896cbaf

--- src/routes/Purchase/WeldEntry/components/WeldEntryDetailTr.js
+++ src/routes/Purchase/WeldEntry/components/WeldEntryDetailTr.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import ItemOperationArea from './ItemOperationArea'
+import ItemOperationArea from 'components/ItemOperationArea'
 
 class WeldEntryDetailTr extends React.Component {
   constructor (props) {
